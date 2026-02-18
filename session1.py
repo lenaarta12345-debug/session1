40e3631cbee6aebffb33ef60e35ccf67fa9d2340
lenaarta12345-debug <lena.arta12345@gmail.com>
2026-02-18 16:46:21 +0100

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,20 @@
+print("Hello","My name is Arta Cani","My favorite subject is Maths")
+
+print("Arta Cani")
+print("18")
+print("Kings Tirana")
+print("My hobby is drawing")
+print("Dream job is to be a software engineerer of my company")
+
+print("Person A:'What is our next class?'")
+print("Person B:'We have english in 204'")
+
+print("          CandyCrash")
+print("Tap anywhere on the screen to start")
+print("LOADING......")
+
+print("   /\\")
+print("  /  \\")
+print(" ______")
+print(  " |","   |")
+print( " ___||_")
