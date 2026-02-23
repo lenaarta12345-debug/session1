7b70cae2c3fad022fb82119e2b66445077ae04bc
lenaarta12345-debug <lena.arta12345@gmail.com>
2026-02-23 16:09:43 +0100

    session2lena.py

---
+++ session2lena.py
@@ -0,0 +1,49 @@
+#create an variable for age,name and favourite color.
+age = 17
+name = "Ilenia"
+favourite_color = "White"
+
+#Print all of them in seperate lines
+print("name:", name)
+print("age:", age)
+print("favourite Color:", favourite_color)
+#Store 3 numbers in different variables and print their sum.
+number1 = 5
+number2 = 7
+number3 = 2
+print(number1+number2+number3)
+
+#Create variables for school name,grade and favourite subject,and write an short introduction.
+school_name = "Luan Hajdaraga"
+grade = 11
+favourite_subject = "English"
+introduction = "Hi,my name is Ilenia,I am 17 years old and I am from Albania."
+
+#Calculate the age of an person if he is born in 1900,store it in a variable and print it.
+current_year = 2026
+birth_year = 1900
+print(current_year-birth_year)
+
+#Convert the 10 kilometers to meters,5 hours into minutes,store them in variables and print them.
+kilometers = 10
+meters = kilometers*1000
+print(meters)
+
+#Take the name of the user as input and print a greeting with their name on it (Hello,[name])
+name = input("Enter your name: ")
+print("Hello,", name)
+
+#Take the name,age and city of the user as input and print a short introduction for them
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+print("Hi, my name is", name + ", I am", age, "years old and I live in", city + ".")
+
+#Take 2 integer numbers as input,print them and their sum.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_numbers = num1 + num2
+
+print("First number:", num1)
+print("Second number:", num2)
+print("Sum:", sum_numbers)
